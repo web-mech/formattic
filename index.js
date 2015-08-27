239de4f7aceca002eabfb7bdde347a9279b89145
webmech <webmech@gmail.com>
2015-08-27 19:46:55 -0400

    add standardizer step

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var finder = require('./lib/finder'),
   beautifier = require('./lib/beautifier'),
+  standardizer = require('./lib/standardizer'),
   q = require('q');
 
 function formatFiles(path, pattern) {
@@ -7,9 +8,17 @@ function formatFiles(path, pattern) {
     return q.allSettled(files.map(beautifier));
   });
 }
+
+function standardizeFiles(path) {
+  return finder(path, '*.js').then(function(files) {
+    return q.allSettled(files.map(standardizer));
+  });
+}
 module.exports = function(path, pattern) {
   pattern = pattern.split('|');
   return q.allSettled(pattern.map(function(pattern) {
     return formatFiles(path, pattern);
-  }));
+  })).then(function() {
+    return standardizeFiles(path);
+  });
 };
